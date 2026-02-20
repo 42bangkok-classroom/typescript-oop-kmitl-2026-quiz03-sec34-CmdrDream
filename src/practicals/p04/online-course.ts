b93fe54429935feff8ec76122598ceb751f87937
CmdrDream <chayakorn11062549@gmail.com>
2026-02-20 17:51:40 +0700

    first

--- src/practicals/p04/online-course.ts
+++ src/practicals/p04/online-course.ts
@@ -1,1 +1,41 @@
 export class OnlineCourse {}
+    public courseName: string;
+    public maxStudents: number;
+    private enrolledStudents: number;
+    private isOpen: boolean;
+
+    constructor(courseName: string, maxStudents: number){
+        this.courseName = courseName;
+        this.maxStudents = maxStudents;
+        this.enrolledStudents = 0;
+        this.isOpen = true;
+    }
+
+    OnlineCourse.getAvailableSeats(): number {
+        return this.maxStudents - this.enrolledStudents;
+    }
+
+    enroll(): boolean {
+        if (this.isOpen && this.enrolledStudents < this.maxStudents) {
+            this.enrolledStudents++;
+            return true;
+        }
+        return false;
+    }
+
+    closeCourse(): void {
+        this.isOpen = false;
+    }
+
+    getAvailableSeats(): number{
+        return this.maxStudents - this.enrolledStudents;
+    }
+
+    getCourseStatus(): string {
+        return this.isOpen ? "Open" : "Closed";
+        
+    }
+
+    canEnroll(): boolean {
+        return this.isOpen && this.enrolledStudents < this.maxStudents;
+    }
