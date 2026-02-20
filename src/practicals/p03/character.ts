7d28f0d8a61bd0e71277c913e357e9935728e61b
CmdrDream <chayakorn11062549@gmail.com>
2026-02-20 18:00:44 +0700

    third

--- src/practicals/p03/character.ts
+++ src/practicals/p03/character.ts
@@ -1,2 +1,14 @@
 export class Character {}
     
+    public name: string;
+    public health: number;
+
+    constructor(name: string) {
+        this.name = name;
+        this.health = 100;
+    }
+
+    getName(): string {
+        return this.name;
+
+    }   
