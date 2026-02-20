98db6faf0219079a17eac508da1178eee876eba7
CmdrDream <chayakorn11062549@gmail.com>
2026-02-20 17:33:53 +0700

    1

--- src/practicals/p01/car.ts
+++ src/practicals/p01/car.ts
@@ -1,1 +1,23 @@
 export class Car {}
+    public Brand: string;
+    public Model: string;
+
+    constructor(brand: string, model: string) {
+        this.Brand = brand;
+        this.Model = model;
+    }
+    Car.start(): void {
+        console.log(`${this.Brand} ${this.Model} is starting...`);
+    }
+
+    Car.accelerate(): void {
+        console.log(`${this.Brand} ${this.Model} is accelerating...`);
+    }
+
+    Car.brake(): void {
+        console.log(`${this.Brand} ${this.Model} is braking...`); 
+    }
+
+    Car.stop(): void {
+        console.log(`${this.Brand} ${this.Model} is stopping...`);
+    }
--- src/practicals/p02/user.ts
+++ src/practicals/p02/user.ts
@@ -1,1 +1,36 @@
 export class User {}
+    public firstName: string;
+    public lastName: string;
+    public age: number;
+    public fullName: string;
+
+    constructor(firstName: string, lastName: string, age: number){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.fullName = `${firstName} ${lastName}`;
+    }
+
+    User.setFirstname(firstName: string): void {
+        this.firstName = firstName;
+        this.fullName = `${this.firstName} ${this.lastName}`;
+    }
+
+    User.setLastname(lastName: string): void {
+        this.lastName = lastName;
+        this.fullName = `${this.firstName} ${this.lastName}`;
+    }
+
+    User.setAge(age: number): void {
+        this.age = age;
+    }
+
+    User.getFullName(): string {
+        return this.fullName;
+    }
+
+    User.getAge(): number {
+        return this.age;
+    }
+
+    static BIRTH_YEAR: number = 2000;
