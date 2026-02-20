e099bf6753ba978e447ef7cc6620731bb3c2c13b
CmdrDream <chayakorn11062549@gmail.com>
2026-02-20 18:47:20 +0700

    earth

--- src/practicals/p04/online-course.ts
+++ src/practicals/p04/online-course.ts
@@ -1,56 +1,38 @@
-export class OnlineCourse {}
-    public courseName: string;
-    public maxStudents: number;
-    private enrolledStudents: number;
-    private isOpen: boolean;
+export class OnlineCourse {
+  public courseName: string;
+  public maxStudents: number;
+  private enrolledStudents: number;
+  private isOpen: boolean;
 
-    constructor(courseName: string, maxStudents: number){
-        this.courseName = courseName;
-        this.maxStudents = maxStudents;
-        this.enrolledStudents = 0;
-        this.isOpen = true;
-    }
+  constructor(courseName: string, maxStudents: number) {
+    this.courseName = courseName;
+    this.maxStudents = maxStudents;
+    this.enrolledStudents = 0;
+    this.isOpen = true;
+  }
 
-    OnlineCourse.getAvailableSeats(): number {
-        return this.maxStudents - this.enrolledStudents;
-    }
+  public getAvailableSeats(): number {
+    return this.maxStudents - this.enrolledStudents;
+  }
 
-    enroll(): boolean {
-        if (this.isOpen && this.enrolledStudents < this.maxStudents) {
-            this.enrolledStudents++;
-            return true;
-        }
-        return false;
+  public enroll(): boolean {
+    if (!this.isOpen) {
+      return false;
     }
 
-    closeCourse(): void {
-        this.isOpen = false;
+    if (this.enrolledStudents >= this.maxStudents) {
+      return false;
     }
 
-    getAvailableSeats(): number{
-        return this.maxStudents - this.enrolledStudents;
-    }
+    this.enrolledStudents++;
+    return true;
+  }
 
-    getCourseStatus(): string {
-        return this.isOpen ? "Open" : "Closed";
-        
-    }
+  public closeCourse(): void {
+    this.isOpen = false;
+  }
 
-    canEnroll(): boolean {
-        return this.isOpen && this.enrolledStudents < this.maxStudents;
-    }
-    
-    const course = new OnlineCourse("Introduction to TypeScript", 30);
-    console.log(course.getCourseStatus());
-    console.log(course.getAvailableSeats());
-    
-    for (let i = 0; i < 30; i++) {
-        course.enroll();
-    }
-    
-    console.log(course.getAvailableSeats());
-    console.log(course.canEnroll());
-    
-    course.closeCourse();
-    console.log(course.getCourseStatus());
-    console.log(course.canEnroll());
+  public getCourseStatus(): string {
+    return this.isOpen ? "Open" : "Closed";
+  }
+}
